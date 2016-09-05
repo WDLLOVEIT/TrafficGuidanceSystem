52fdc7e61cf77e8b64e5929badccb6dc0e2a20cd
doudoubudou <doufang@DESKTOP-OUHRLR8>
2016-09-05 11:10:25 +0800

    SQL更新

--- TrafficGuidanceSystem/trafficguidancesystem.sql
+++ TrafficGuidanceSystem/trafficguidancesystem.sql
@@ -1,12 +1,4 @@
-﻿# Host: localhost  (Version 5.5.21)
-# Date: 2016-08-31 08:36:48
-# Generator: MySQL-Front 5.3  (Build 5.21)
 
-/*!40101 SET NAMES utf8 */;
-
-#
-# Structure for table "admin"
-#
 
 CREATE TABLE `admin` (
   `adminId` int(11) NOT NULL AUTO_INCREMENT,
@@ -15,15 +7,10 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`adminId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-#
-# Data for table "admin"
-#
+
 
 INSERT INTO `admin` VALUES (1,'admin','doudou'),(2,'1','1');
 
-#
-# Structure for table "coordinate"
-#
 
 CREATE TABLE `coordinate` (
   `coordinateId` int(11) NOT NULL AUTO_INCREMENT COMMENT '位置',
@@ -32,14 +19,7 @@ CREATE TABLE `coordinate` (
   PRIMARY KEY (`coordinateId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#
-# Data for table "coordinate"
-#
 
-
-#
-# Structure for table "map"
-#
 
 CREATE TABLE `map` (
   `mapId` int(11) NOT NULL AUTO_INCREMENT,
@@ -48,14 +28,8 @@ CREATE TABLE `map` (
   PRIMARY KEY (`mapId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#
-# Data for table "map"
-#
+Structure for table "user"
 
-
-#
-# Structure for table "user"
-#
 
 CREATE TABLE `user` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,8 +38,6 @@ CREATE TABLE `user` (
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
-#
-# Data for table "user"
-#
+
 
 INSERT INTO `user` VALUES (1,'doudou','1234565'),(3,'u','u'),(4,'dddddddd','ddddddd'),(5,'234234','2342342'),(6,'doudoubudou','doudoudou'),(7,'doudoubudou','doudoudou'),(8,'1','1');
